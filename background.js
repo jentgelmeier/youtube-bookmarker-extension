212e9fc1bd62073d7d0aa85056313846c4dab197
Jason Entgelmeier <jasonentgelmeier@Emilys-MacBook-Pro-2.local>
2025-05-13 18:14:51 -0400

    fix bug that prevented the timestamps being added for correct videoIds

--- background.js
+++ background.js
@@ -1,12 +1,27 @@
-chrome.tabs.onUpdated.addListener((tabId, tab) => {
-  if (tab.url && tab.url.includes("youtube.com/watch")) {
-    const queryParameters = tab.url.split("?")[1];
-    const urlParameters = new URLSearchParams(queryParameters)
+/*
+This code will listen for both chrome.tabs.onUpdated and chrome.tabs.onActivated events. There was a bug with the original code where the videoId was not being sent because the message was only being sent for the onUpdate event. This should ensure that the message is being sent to contentScript.js when the URL is updated, and also when you switch between different tabs.
+*/
+
+const sendMessageToContentScript = (tabId, url) => {
+  if (url && url.includes("youtube.com/watch")) {
+    const queryParameters = url.split("?")[1];
+    const urlParameters = new URLSearchParams(queryParameters);
 
     chrome.tabs.sendMessage(tabId, {
       type: "NEW",
-      videoId: urlParameters.get("v")
+      videoId: urlParameters.get("v"),
     });
+  }
+};
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === 'complete') {
+    sendMessageToContentScript(tabId, tab.url);
   }
 });
 
+chrome.tabs.onActivated.addListener((activeInfo) => {
+  chrome.tabs.get(activeInfo.tabId, (tab) => {
+    sendMessageToContentScript(activeInfo.tabId, tab.url);
+  });
+});
