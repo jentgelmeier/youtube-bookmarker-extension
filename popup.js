6846eb294f76e2fdc7f77b08751c37a4bd6fc86f
Jason Entgelmeier <jasonentgelmeier@Emilys-MacBook-Pro-2.local>
2025-05-13 18:09:53 -0400

    add to contentScript.js, popup.js

--- popup.js
+++ popup.js
@@ -1,7 +1,34 @@
-// adding a new bookmark row to the popup
-const addNewBookmark = () => {};
+import { getActiveTabURL } from "./utils.js";
 
-const viewBookmarks = () => {};
+// adding a new bookmark row to the popup
+const addNewBookmark = (bookmarksElement, bookmark) => {
+  const bookmarkTitleElement = document.createElement("div");
+  const newBookmarkElement = document.createElement("div");
+
+  bookmarkTitleElement.textContent = bookmark.desc;
+  bookmarkTitleElement.className = "bookmark-title";
+
+  newBookmarkElement.id = "bookmark-" + bookmark.time;
+  newBookmarkElement.className = "bookmark";
+  newBookmarkElement.setAttribute("timestamp", bookmark.time);
+
+  newBookmarkElement.appendChild(bookmarkTitleElement);
+  bookmarksElement.appendChild(newBookmarkElement);
+};
+
+const viewBookmarks = (currentBookmarks = []) => {
+  const bookmarksElement = document.getElementById("bookmarks");
+  bookmarksElement.innerHTML = "";
+
+  if (currentBookmarks.length) {
+    for (let i = 0; i < currentBookmarks.length; i++) {
+      const bookmark = currentBookmarks[i];
+      addNewBookmark(bookmarksElement, bookmark);
+    }
+  } else {
+    bookmarksElement.innerHTML = "<i class='row'>No bookmarks to show</div>";
+  }
+};
 
 const onPlay = e => {};
 
@@ -9,4 +36,22 @@ const onDelete = e => {};
 
 const setBookmarkAttributes =  () => {};
 
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", async () => {
+  const activeTab = await getActiveTabURL();
+  const queryParameters = activeTab.url.split("?")[1];
+  const urlParameters = new URLSearchParams(queryParameters);
+
+  const currentVideo = urlParameters.get("v");
+  
+  if (activeTab.url.includes("youtube.com/watch") && currentVideo) {
+  
+    chrome.storage.sync.get([currentVideo]).then((data) => {
+      const currentVideoBookmarks = data[currentVideo] ? JSON.parse(data[currentVideo]) : [];
+      viewBookmarks(currentVideoBookmarks);
+    });
+  } else {
+    const container = document.getElementsByClassName("container")[0];
+    
+    container.innerHTML = "<div class='title'>This is not a youtube video page.</div>";
+  }
+});
